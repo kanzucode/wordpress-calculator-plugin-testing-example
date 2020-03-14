f7e2e959cb20827e1e58ae5fea2d83a2dce7fad5
Peter Kakoma <kakoma@kanzucode.com>
2020-03-14 10:11:10 +0300

    Add initial plugin file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # wordpress-calculator-plugin-testing-example
 A sample WordPress plugin to illustrate automated testing using Codeception
+
+## Installation
+
+Clone this repo into your plugins folder, run composer & then activate the plugin in your WordPress dashboard
+
+```
+git clone git@github.com:kanzucode/wordpress-calculator-plugin-testing-example.git
+cd wordpress-calculator-plugin-testing-example
+composer install
+```
---
+++ wp-calculator.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Plugin Name:       Kanzu Calculator
+ * Plugin URI:        https://kanzucode.com
+ * Description:       A sample WordPress plugin to illustrate automated testing using Codeception
+ * Version:           1.0.0
+ * Author:            Kanzu Code
+ * Author URI:        https://kanzucode.com
+ * Requires PHP:      7.0
+ * Text Domain:       kz-wp-calculator
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+
+
+if (! defined('ABSPATH')) {
+    exit;
+} // End if().
+
+if (! class_exists('Kanzu_WP_Calculator')) {
+    final class Kanzu_WP_Calculator
+    {
+
+
+        /**
+         * Reference to plugin version
+         *
+         * @var string
+         */
+        public $version = '1.0.0';
+
+
+        /**
+         * Variable that holds the one and only instance of the calculator
+         *
+         * @var Kanzu_WP_Calculator
+         */
+        private static $_instance = null;
+ 
+
+        /**
+         * Load the global Calculator instance
+         *
+         * @return Kanzu_WP_Calculator
+         */
+        public static function get_instance()
+        {
+            if (is_null(self::$_instance)) {
+                self::$_instance = new self();
+            }
+            return self::$_instance;
+        }
+
+        /**
+         * Cloning is forbidden.
+         *
+         * @since 1.8.0
+         */
+        public function __clone()
+        {
+            _doing_it_wrong(__FUNCTION__, __('Cheatin&#8217; huh?', 'kz-wp-starter-plugin'), $this->version);
+        }
+
+        /**
+         * Unserializing instances of this class is forbidden.
+         *
+         * @since 1.8.0
+         */
+        public function __wakeup()
+        {
+            _doing_it_wrong(__FUNCTION__, __('Cheatin&#8217; huh?', 'kz-wp-starter-plugin'), $this->version);
+        }
+
+        public function __construct()
+        {
+            $this->define_constants();
+            $this->includes();
+
+        }
+
+        private function define_constants()
+        {
+            $this->define('KZ_WP_CALCULATOR_PLUGIN_VERSION', $this->version);
+            $this->define('KZ_WP_CALCULATOR_PLUGIN_DIR', plugin_dir_path(__FILE__));
+            $this->define('KZ_WP_CALCULATOR_PLUGIN_PLUGIN_URL', plugin_dir_url(__FILE__));
+            $this->define('KZ_WP_CALCULATOR_PLUGIN_PLUGIN_FILE', __FILE__);
+        }
+
+        /**
+         * Define constant if not already set.
+         *
+         * @param  string      $name
+         * @param  string|bool $value
+         */
+        private function define($name, $value)
+        {
+            if (! defined($name)) {
+                define($name, $value);
+            }
+        }
+
+        private function includes()
+        {
+            require_once KZ_WP_CALCULATOR_PLUGIN_DIR . '/vendor/autoload.php';
+        }
+    }
+
+    function Kanzu_WP_Calculator()
+    {
+        return Kanzu_WP_Calculator::get_instance();
+    }
+
+    Kanzu_WP_Calculator();
+}// End if().
